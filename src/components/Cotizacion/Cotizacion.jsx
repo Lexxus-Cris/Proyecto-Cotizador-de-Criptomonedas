f4a074f167cae56c0c898c1334cf460606d1cc89
Uriel Cristino <urielcris.97@gmail.com>
2021-09-09 14:23:39 -0500

    Agregando CSS a Cotizacion

--- src/components/Cotizacion/Cotizacion.jsx
+++ src/components/Cotizacion/Cotizacion.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap';
 
+import { ResultsCont, Precio, Info } from './styles';
+
 
 const Cotizacion = ({results, moneda, criptomoneda, setModal, modal}) => {
+
+   const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, LASTUPDATE } = results;
 
    // Si el componente recibe un objeto vacio no retorna nada
    if (Object.keys(results).length === 0) return null;
@@ -14,19 +18,24 @@ const Cotizacion = ({results, moneda, criptomoneda, setModal, modal}) => {
          <ModalHeader>
             Cotizacion de {criptomoneda} a {moneda}
          </ModalHeader>
+
          <ModalBody>
-            <p>El precio es: <span>{results.PRICE}</span></p>
-            <p>Precio más alto del día: <span>{results.HIGHDAY}</span></p>
-            <p>Precio más bajo del día: <span>{results.LOWDAY}</span></p>
-            <p>Variación últimas 24 horas: <span>{results.CHANGEPCT24HOUR}</span></p>
-            <p>Última Actualización: <span>{results.LASTUPDATE}</span></p>
+            <ResultsCont>
+               <Precio>El precio es: <span>{PRICE}</span></Precio>
+               <Info>Precio más alto del día: <span>{HIGHDAY}</span></Info>
+               <Info>Precio más bajo del día: <span>{LOWDAY}</span></Info>
+               <Info>Variación últimas 24 horas: <span>{CHANGEPCT24HOUR}</span></Info>
+               <Info>Última Actualización: <span>{LASTUPDATE}</span></Info>
+            </ResultsCont>
          </ModalBody>
+
          <ModalFooter>
             <Button
                onClick={() => setModal(false)}
                color="primary"
             >Cerrar</Button>
          </ModalFooter>
+
       </Modal>
    )
 }
---
+++ src/components/Cotizacion/styles.js
@@ -0,0 +1,24 @@
+import styled from "@emotion/styled";
+
+export const ResultsCont = styled.div`
+   display: flex;
+   align-content: center;
+   flex-flow: column nowrap;
+   font-family: Arial, Helvetica, sans-serif;
+`;
+
+export const Info = styled.p`
+   font-size: 18px;
+
+   span {
+      font-weight: bold;
+   }
+`;
+
+export const Precio = styled.span`
+   font-size: 25px;
+
+   span {
+      font-weight: bold;
+   }
+`;

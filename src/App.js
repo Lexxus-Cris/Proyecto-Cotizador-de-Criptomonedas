5d1f90c4ea169e34a952fe96ccef14a16e3054ef
Uriel Cristino <urielcris.97@gmail.com>
2021-09-09 12:08:07 -0500

    Consultando la API y trayendo los valores

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
+
 import { Contenedor, Imagen, Heading } from "./styles";
+
 import image from './img/cryptomonedas.png';
 
 import Form from './components/Form/Form';
@@ -11,10 +14,17 @@ function App() {
 
   useEffect(() => {
 
-    // Evitamos el primer renderizado del componente
-    if (moneda === ' ')  return;
+    const cotizarCripto = async () => {
+      // Evitamos el primer renderizado del componente
+      if (coin === '')  return;
+  
+      // Consultar API
+      const API_URL = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptoCoin}&tsyms=${coin}`;
+      const response = await axios.get(API_URL);
+    }
 
-    
+    cotizarCripto()
+
   }, [coin, criptoCoin])
 
   return (
